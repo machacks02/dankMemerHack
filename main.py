4789803addad63b5469625ad946b5ddd046ad77c
Auxtal <real.auxtal@gmail.com>
2021-07-08 12:42:22 +0800

    made improvements

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import discord
+from discord.ext import commands
+from discord import utils
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
+TOKEN = 'PUT YOUR APPLICATIONS TOKEN INSIDE THE QUOTATION MARKS'
 
 @client.event
 async def on_ready():
-    print('You are logged in as {0.user}'.format(client))
+    print(f'You are logged in as {client.user.name} (ID: {client.user.id})')
 
 @client.event
 async def on_message(message):
+    message_content = message.clean_content
+    message_content = utils.remove_markdown(message_content)
+
     file = open('messages.txt', 'a')
-    file.write(message.author.name + ': ' + message.content + '\n')
-    if message.author.name == 'Dank Memer':
+    file.write(f'{message.author.name}: {message.content}\n')
+
+    if message.author.id == 270904126974590976:
         file2 = open('recent.txt', 'w')
-        file2.write(message.author.name +': ' + message.content)
+        file2.write(f'{message.author.name}: {message.content}\n')
 
-client_secret = 'PUT YOUR CLIENT SECRET HERE'
-
-client.run(client_secret)
+client.run(TOKEN)
